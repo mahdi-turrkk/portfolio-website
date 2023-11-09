7005613b202f8189a62ad7bc884a5ea75e0d84de
mahdi_turrkk <mahdichavoshi14@gmail.com>
2023-11-09 18:40:14 +0330

    hero section finished

---
+++ src/components/heroSection.vue
@@ -0,0 +1,86 @@
+<template>
+  <div
+      class="min-h-screen flex md:flex-row md:justify-between md:px-60 justify-center items-center pt-16 flex-col-reverse relative">
+    <img src="../assets/bg-filter.png" class="absolute w-screen h-screen top-0 right-0 z-0" :class="{'opacity-80' : !isDark , 'opacity-5' : isDark}">
+    <div class="mt-10 md:mt-0 px-12 md:px-16 z-10">
+      <div class="uppercase text-2xl text-primary-3">
+        {{ locale.greeting }}
+      </div>
+      <div class="flex items-end mt-4">
+        <div class="text-5xl sm:text-6xl text-info-3">
+          {{ locale.name }}
+        </div>
+        <div class="text-xl mr-2 text-info-1">
+          {{ isRtl ? locale.is : '' }}
+        </div>
+      </div>
+      <div class="uppercase text-primary-3 text-2xl sm:text-3xl" :class="{'mt-8' : isRtl , 'mt-4' : !isRtl}">
+        {{ locale.role }}
+      </div>
+      <div class="text-base md:text-lg text-info-1 max-w-lg text-justify mt-4">{{ locale.aboutSummary }}</div>
+      <div class="flex mt-6">
+        <button
+            class="rounded-xl bg-primary-2 text-gray-100 uppercase px-6 py-2 hover:bg-primary-3 transition-all duration-200"
+            :class="{'ml-4' : isRtl , 'mr-4' : !isRtl}">{{ locale.downloadCV }}
+        </button>
+        <button
+            class="rounded-xl border-2 border-primary-3 bg-background-2 hover:bg-background-1 text-info-1 uppercase px-6 py-2"
+            @click="router.push('#Projects')">{{ locale.seeProjects }}
+        </button>
+      </div>
+      <div class="flex mt-4">
+        <a v-for="socialMedia in socialMedias" :href="socialMedia.link" class="decoration-0"
+           :class="{'ml-4' : isRtl , 'mr-4' : !isRtl}">
+          <button class="rounded-full hover:bg-background-2 w-12 h-12 flex justify-center items-center">
+            <i class="pi text-primary-3 text-2xl" :class="socialMedia.iconClass"/>
+          </button>
+        </a>
+      </div>
+    </div>
+    <div class="z-10">
+      <hero-section-image/>
+    </div>
+  </div>
+</template>
+<script setup>
+import router from "../router/index.js";
+import HeroSectionImage from "./heroSectionImage.vue";
+import {computed, ref} from "vue";
+import {useLocalization} from "../store/localizationStore.js";
+import {useDataStore} from "../store/dataStore.js";
+
+let locale = computed(() => {
+  return useLocalization().getLocale
+})
+
+let isRtl = computed(() => {
+  return useLocalization().getDirection === 'rtl'
+})
+
+let isDark = computed(() => {
+  return useDataStore().getDarkStatus
+})
+
+const socialMedias = ref([
+  {
+    title: 'instagram',
+    iconClass: {'pi-instagram': true},
+    link: 'https://instagram.com/mahdi_turrkk'
+  },
+  {
+    title: 'github',
+    iconClass: {'pi-github': true},
+    link: 'https://github.com/mahdi-turrkk'
+  },
+  {
+    title: 'linkedin',
+    iconClass: {'pi-linkedin': true},
+    link: 'https://linkedin.com/in/mahdi-chavoshi'
+  },
+  {
+    title: 'telegram',
+    iconClass: {'pi-telegram': true},
+    link: 'https://t.me/mahdi_turrkk'
+  },
+])
+</script>
---
+++ src/components/heroSectionImage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex w-max">
+    <div class="relative z-0 shape bg-primary-3 h-[450px] w-[345px] md:h-[575px] md:w-[500px] bg-opacity-70"></div>
+    <div class="relative z-0 shape h-[550px] w-[400px] md:h-[675px] md:w-[550px] -mt-[98px] md:-mt-[98px] overflow-y-hidden flex justify-center items-end" :class="{'-mr-[373px] md:-mr-[527px]' : isRtl , '-ml-[373px] md:-ml-[527px]' : !isRtl}">
+      <img src="/src/assets/selfImage.png" class="z-20 h-[500px] md:h-[650px]" :alt="useLocalization().getLocale.name">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.shape {
+  border-radius: 29% 71% 70% 30% / 30% 30% 70% 70%;
+}
+
+</style>
+<script setup>
+import {useLocalization} from "../store/localizationStore.js";
+import {computed} from "vue";
+
+let isRtl = computed(() => {
+  return useLocalization().getDirection === 'rtl'
+})
+</script>
