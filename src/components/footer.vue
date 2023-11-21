e5fe86266ae6f868e250f2cad0175ea9f4c3fd82
mahdi_turrkk <mahdichavoshi14@gmail.com>
2023-11-21 09:20:06 +0330

    footer finished

---
+++ src/components/footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+      class="relative">
+    <div class="border-t-2 border-info-1 mx-4"></div>
+    <div class="flex justify-center items-center py-2 text-info-1">
+      <div>
+        Designed and Developed By
+        <span class="text-info-2">
+                <a href="https://github.com/mahdi-turrkk">Mahdi Chavoshi</a>
+      </span>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+</script>
