328bac24fff549cff1de6fd61faefcee8057acb4
mahdi_turrkk <mahdichavoshi14@gmail.com>
2023-11-21 09:20:17 +0330

    homepage added

--- src/pages/homePage/index.vue
+++ src/pages/homePage/index.vue
@@ -1,13 +1,23 @@
 <template>
-  <div>
-    home page
-  </div>
+  <default>
+    <hero-section/>
+    <about-section/>
+    <skills-section/>
+    <background-section/>
+    <projects-section/>
+    <contact-section/>
+    <Footer/>
+  </default>
 </template>
 
 <script setup>
+import Default from "../../layouts/default.vue";
+import HeroSection from "../../components/heroSection.vue";
+import AboutSection from "../../components/aboutSection.vue";
+import SkillsSection from "../../components/skillsSection.vue";
+import BackgroundSection from "../../components/backgroundSection.vue";
+import ProjectsSection from "../../components/projectsSection.vue";
+import ContactSection from "../../components/contactSection.vue";
+import Footer from "../../components/footer.vue";
 
 </script>
-
-<style scoped>
-
-</style>
